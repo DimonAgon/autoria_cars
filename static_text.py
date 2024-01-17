67543da95620c66a0ad4981cb8defa9073477e80
DimonAgon <dimon2003agon@gmail.com>
2024-01-17 20:46:27 +0200

    add bot logging messages text, add chat messages text

---
+++ static_text.py
@@ -0,0 +1,26 @@
+
+#chat messages
+search_demand_url_request_chat_message = f"please enter search url alike\n ```https://auto.ria.com/<country_code>/search/<filters>```"
+search_demand_submission_initiated_chat_message = "search demand submission is being processed"
+search_demand_submission_success_chat_message = "search demand was submitted"
+search_demand_submission_failure_chat_message = "your demand was not submitted"
+autoria_search_url_authenticity_validation_success_chat_message = "autoria-search-url authenticity validation successful"
+autoria_search_url_authenticity_validation_failure_chat_message = "autoria-search-url authenticity validation failed"
+autoria_search_url_points_on_real_source_validation_success_chat_message = "autoria-search-url points-on-real-source validation successful"
+autoria_search_url_points_on_real_source_validation_failure_chat_message = "autoria search url points-on-real-source validation failed"
+is_admin_authorization_failure_chat_message = "is-admin authorization failed"
+#c̶h̶a̶t̶ ̶m̶e̶s̶s̶a̶g̶e̶s̶
+
+#logging
+on_search_demand_url_request_logging_info_message = "search url requested from user {} of {}"
+search_demand_submission_initiated_logging_info_message = "{} search demand submission initiated for user {} of {}"
+search_demand_on_submission_success_logging_info_message = "{} search demand submission successful for user {} of {}"
+search_demand_on_submission_failure_logging_error_message = "{} search demand submission failed for user {} of {}"
+autoria_search_url_authenticity_validation_success_logging_info_message = "{} autoria-search-url authenticity validation successful"
+autoria_search_url_authenticity_validation_failure_logging_error_message = "{} autoria-search-url authenticity validation failed"
+autoria_search_url_points_on_real_source_validation_success_logging_info_message = "{} autoria-search-url points-on-real-source validation successful"
+autoria_search_url_points_on_real_source_validation_failure_logging_error_message = "{} autoria-search-url points-on-real-source validation failed"
+is_admin_authorization_success_logging_info_message = "user {} is-admin authorization successful at {}"
+is_admin_authorization_failure_logging_info_message = "user {} is-admin authorization failed at {}"
+#l̶o̶g̶g̶i̶n̶g̶
+
