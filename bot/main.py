25e20d1ab59a8ae67322605c99ef0d7c04eb0578
DimonAgon <dimon2003agon@gmail.com>
2024-01-17 18:34:05 +0200

    add bot main

---
+++ bot/main.py
@@ -0,0 +1,28 @@
+
+import asyncio
+
+from __init__ import *
+from bot import bot
+from middleware import *
+from db.session_maker import session_maker
+
+
+def set_all_middleware():
+    dispatcher.update.middleware(DbSessionMiddleware(session_pool=session_maker))
+
+async def deploy_bot():
+    await dispatcher.start_polling(bot)
+    await dispatcher.stop
+    await bot.session.close()
+
+def main():
+
+    set_all_middleware()
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.create_task(deploy_bot())
+    loop.run_forever()
+
+if __name__ == '__main__':
+    main()
