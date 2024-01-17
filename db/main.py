f8b544d5db3628d1286ffcba0a0cfde78630c2f4
DimonAgon <dimon2003agon@gmail.com>
2024-01-17 18:23:32 +0200

    separate session-maker

--- db/main.py
+++ db/main.py
@@ -9,7 +9,6 @@ from engine import engine
 async def main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 if __name__ == "__main__":
     asyncio.run(main())
