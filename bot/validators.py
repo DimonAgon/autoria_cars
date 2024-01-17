9f84fc69542a6d7c1854e51d9ef83bf09c96ab18
DimonAgon <dimon2003agon@gmail.com>
2024-01-17 20:51:14 +0200

    add autoria-search-url-points-on-real-source validator

--- bot/validators.py
+++ bot/validators.py
@@ -1,4 +1,5 @@
 
+import requests
 
 from static_text import *
 from re_patterns import autoria_authentic_search_url_re_pattern
@@ -17,3 +18,17 @@ def validate_autoria_search_url_is_authentic(search_url: str) -> bool:
         logging.error(autoria_search_url_authenticity_validation_failure_logging_error_message.format(search_url))
 
     return search_url_authenticity
+
+
+def validate_autoria_search_url_points_on_real_source(search_url: str) -> bool:
+
+    response = requests.get(search_url)
+    response_success_status = response.ok
+
+    if response_success_status:
+        logging.info(autoria_search_url_points_on_real_source_validation_success_logging_info_message.format(search_url))
+
+    else:
+        logging.error(autoria_search_url_points_on_real_source_validation_failure_logging_error_message.format(search_url))
+
+    return response_success_status
